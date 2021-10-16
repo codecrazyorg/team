a1d243eb72da9949fe4185e8fe073a2b29e7b6a7
rflickner <92646315+rflickner@users.noreply.github.com>
2021-10-16 18:42:00 +0000

    hello world java test code

---
+++ test/HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+Java Hello World example.
+*/
+
+/*
+Here
+is another comment.
+*/ 
+public class HelloWorldExample{
+ 
+  public static void main(String args[]){
+ 
+    /*
+    Use System.out.println() to print on console.
+    */
+    System.out.println("Hello World !");
+  
+  }
+ 
+}
+ 
+/*
+ 
+OUTPUT of the above given Java Hello World Example would be :
+ 
+Hello World !
+ 
+*/
